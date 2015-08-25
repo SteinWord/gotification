2f68caf05d94d4e38d901e008ffe582a0c225742
SteinWord <fmodqueensya+steinword@gmail.com>
2015-08-26 04:01:52 +0900

    update
    update

--- gotification.go
+++ gotification.go
@@ -33,49 +33,85 @@ func (c *Config) Set() {
 func (n *Notification) Notify() bool {
 	task1 := make(chan bool)
 	task2 := make(chan bool)
+	err1 := []string{}
+	err2 := []string{}
 	var wg1 sync.WaitGroup
 	var wg2 sync.WaitGroup
 	go func() {
 		wg1.Add(len(n.AndroidReceivers))
-		for i, v := range n.AndroidReceivers {
-			go test(v, n.Message, i, &wg1)
+		for _, v := range n.AndroidReceivers {
+			// go gcmNotify(v, n.Message, &wg1)
+			go gcmNotify(v, n.Message, &err1, &wg1)
 		}
 		wg1.Wait()
 		task1 <- true
 	}()
 	go func() {
 		wg2.Add(len(n.IOSReceivers))
-		for i, v := range n.IOSReceivers {
-			go test(v, n.Message, i, &wg2)
+		for _, v := range n.IOSReceivers {
+			// go apnNotify(v, n.Message, &wg2)
+			go apnNotify(v, n.Message, &err2, &wg2)
 		}
 		wg2.Wait()
 		task2 <- true
 	}()
 	<-task1
 	<-task2
-	println("finish task")
+	for _, v := range err1 {
+		println(v)
+	}
+	for _, v := range err2 {
+		println(v)
+	}
 	return true
 }
 
-func test(att string, msg string, i int, wg *sync.WaitGroup) bool {
+func test(att string, msg string, i int, wg *sync.WaitGroup, err *[]int) (status bool) {
 	content := []byte(fmt.Sprintf("%s %s %s\n", time.Now(), att, msg))
 	ioutil.WriteFile("/Users/QsF/tmp/go-testfile.log", content, 0755)
 	fmt.Println(time.Now(), att, msg, i)
 	time.Sleep(1 * time.Second)
+	if i == 32 || i == 33 || i == 564 || i == 921 || i == 612 || i == 741 || i == 742 || i == 743 {
+		status = false
+		*err = append(*err, i)
+	} else {
+		status = true
+	}
 	wg.Done()
-	return true
+	return
 }
 
-func apnNotify(att string, msg string) {
-	apns.LoadCertificateFile(false, apnauth)
+func apnNotify(att string, msg string, errHandle *[]string, wg *sync.WaitGroup) (status bool) {
+	status = true
+	err := apns.LoadCertificateFile(false, apnauth)
+	if err != nil {
+		status = false
+	}
 	payload := &apns.Notification{Alert: fmt.Sprintf(msg), Badge: 0, Sandbox: true}
 	payload.SetExpiryDuration(24 * time.Hour)
-	payload.SendTo(att)
+	err = payload.SendTo(att)
+	if err != nil {
+		status = false
+	}
+	wg.Done()
+	if status == false {
+		*errHandle = append(*errHandle, att)
+	}
+	return
 }
 
-func gcmNotify(att string, msg string) {
+func gcmNotify(att string, msg string, errHandle *[]string, wg *sync.WaitGroup) (status bool) {
+	status = true
 	d := map[string]interface{}{"message": msg}
 	sender := &gcm.Sender{ApiKey: gcmauth}
 	data := gcm.NewMessage(d, att)
-	sender.Send(data, 2)
+	_, err := sender.Send(data, 0)
+	if err != nil {
+		status = false
+	}
+	wg.Done()
+	if status == false {
+		*errHandle = append(*errHandle, att)
+	}
+	return
 }
